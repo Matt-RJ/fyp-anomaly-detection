fb658fea3daaef4dc4482391d974caee6d268199
Matt-RJ <mantasrajackas@gmail.com>
2021-03-14 21:03:53 +0000

    Added isolation forest notebook and utility functions

--- util.py
+++ util.py
@@ -40,7 +40,7 @@ def json_to_pandas(filepath):
         for i, df in enumerate(data):
             # Timestamp conversion
             for j, timestamp in enumerate(data[i]["Timestamps"]):
-                data[i]["Timestamps"][j] = pd.to_datetime(timestamp)
+                data[i]["Timestamps"][j] = pd.to_datetime(timestamp, utc=True)
 
             df = pd.DataFrame({
                 "Timestamps": data[i]["Timestamps"],
@@ -48,3 +48,45 @@ def json_to_pandas(filepath):
             })
             dfs[data[i]["Label"]] = df
     return dfs
+
+# Loads service release info from a json file
+def load_releases(filepath):
+    with open(filepath) as f:
+        df = pd.DataFrame(json.load(f))
+
+        # Timestamp conversion
+        df['timestamp'] = df['timestamp'].apply(lambda x: pd.to_datetime(x, unit='s', utc=True))
+        
+        # Cleanup
+        df = df.rename(columns={'timestamp': 'Timestamps', 'serviceName': 'ServiceNames'})
+    return df
+
+# Adds a new feature 'PostRelease' to df, based on the nearest release timestamps in df_releases
+def calculate_postrelease_feature(df, df_releases):
+    df['PostRelease'] = 0
+
+    # Removing any releases that fall outside of the df metric time range
+    earliest_timestamp = df.Timestamps.iloc[1]
+    latest_timestamp = df.Timestamps.iloc[-1]
+    print('Earliest in-scope release date: ', earliest_timestamp)
+    print('Latest in-scope release date: ', latest_timestamp)
+
+    out_of_scope_release_indices = [] # Release indices to be dropped
+    for i, release_timestamp in enumerate(df_releases.Timestamps):
+        if (release_timestamp < earliest_timestamp or release_timestamp > latest_timestamp):
+            out_of_scope_release_indices.append(i)
+
+    df_releases = df_releases.drop(out_of_scope_release_indices)
+    print(f'Dropped {len(out_of_scope_release_indices)} out of scope release date(s)')
+
+    # Adding PostRelease feature, where the 
+    for release_date in df_releases.Timestamps:
+        closest = pd.Timedelta.max
+        for i, row in enumerate(df.Timestamps):
+            timedelta = abs(release_date - row)
+            if (timedelta < closest):
+                closest = timedelta
+            else:
+                df.loc[i, 'PostRelease'] = 1
+                break
+    return df
